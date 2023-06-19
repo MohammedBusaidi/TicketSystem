81452199216a77dfb3b3dcf7860824667bfa0c26
Mohammed <moody.busaidi99@gmail.com>
2023-06-19 11:56:19 +0400

    set the created date and is active in ticket

--- src/main/java/com/ticketSystem/ticketSystem/Services/TicketService.java
+++ src/main/java/com/ticketSystem/ticketSystem/Services/TicketService.java
@@ -5,12 +5,17 @@ import com.ticketSystem.ticketSystem.Repositories.TicketRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
 @Service
 public class TicketService {
     @Autowired
     TicketRepository ticketRepository;
 
     public Ticket createTicket(Ticket ticket){
+        LocalDateTime now = LocalDateTime.now();
+        ticket.setCreatedDate(now);
+        ticket.setActive(true);
         return ticketRepository.save(ticket);
     }
 }
