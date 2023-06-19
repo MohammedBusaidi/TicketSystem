a3c4233dfcc8322f84d92b3b4dcd58f550064830
Mohammed <moody.busaidi99@gmail.com>
2023-06-19 11:54:37 +0400

    set the created date and active in user

--- src/main/java/com/ticketSystem/ticketSystem/Services/UserService.java
+++ src/main/java/com/ticketSystem/ticketSystem/Services/UserService.java
@@ -5,12 +5,17 @@ import com.ticketSystem.ticketSystem.Repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
 @Service
 public class UserService {
     @Autowired
     UserRepository userRepository;
 
     public User createUser(User user) {
+        LocalDateTime now = LocalDateTime.now();
+        user.setCreatedDate(now);
+        user.setActive(true);
         return userRepository.save(user);
     }
 }
